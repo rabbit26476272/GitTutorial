2f882c7959b3882a2d8def3229e69387df87426a
a26476272 <t107590015@ntut.org.tw>
2020-03-07 17:43:11 +0800

    新增"TOMEI!!!!!!"功能

---
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -0,0 +1,13 @@
+﻿// GitTutorial.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include "pch.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "TOMEI!!!!!" << endl;
+	return 0;
+}
